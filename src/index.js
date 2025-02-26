75b48e5c05c577c9b853db6b9c73d047f5cf41b4
Udbhav <udbhavkumar54@gmail.com>
2025-02-26 20:32:41 +0530

    setup project file

---
+++ src/index.js
@@ -0,0 +1,41 @@
+//  required('dotenv').config({path: './env'})
+import dotenv from "dotenv";
+import connectDB from "./db/index.js";
+
+dotenv.config({
+  path: "./env",
+});
+
+// 2nd approach
+
+connectDB();
+
+/*
+ import express from "express"
+ const app = express()
+
+ // 1st approach
+//  function connectDB(){}
+
+
+//  connectDB()
+
+;(async() => {
+  try {
+    await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+    app.on("error", (error)=>{
+      console.log("ERROR: ", error);
+      throw error
+    })
+
+    app.listen(process.env.PORT, ()=>{
+      console.log(`App is listening on port ${process.env.PORT}`);
+      
+    })
+
+  } catch (error) {
+    console.log("Error: ", error)
+    throw error
+  }
+})()
+  */
